764a0591c8129333685c79fca92c014d0aae3c0e
Hp-0512 <hps13145@gmail.com>
2026-02-24 14:33:40 +0530

    Update firebaseAdmin.js

--- Software Encyclopedia_All/Backend/firebaseAdmin.js
+++ Software Encyclopedia_All/Backend/firebaseAdmin.js
@@ -10,13 +10,38 @@
 
 // module.exports = db;
 
+// const admin = require("firebase-admin");
+
+// try {
+//     const serviceAccount = require("./serviceAccountKey.json");
+
+//     admin.initializeApp({
+//         credential: admin.credential.cert(serviceAccount),
+//     });
+
+//     const db = admin.firestore();
+
+//     module.exports = {
+//         admin,
+//         db,
+//     };
+
+//     console.log("Firebase Admin Initialized Successfully");
+
+// } catch (error) {
+//     console.error("Firebase Admin Error:", error);
+// }
+
+
 const admin = require("firebase-admin");
 
 try {
-    const serviceAccount = require("./serviceAccountKey.json");
-
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
+        credential: admin.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        }),
     });
 
     const db = admin.firestore();
