8a87f636de369e9c9ec785ced7bf82d70449d3bb
Hp-0512 <hps13145@gmail.com>
2026-02-24 17:33:34 +0530

    Update firebaseAdmin.js

--- Software Encyclopedia_All/Backend/firebaseAdmin.js
+++ Software Encyclopedia_All/Backend/firebaseAdmin.js
@@ -29,9 +29,9 @@ try {
     if (!admin.apps.length) {
         admin.initializeApp({
             credential: admin.credential.cert({
-                project_id: process.env.FIREBASE_PROJECT_ID,
-                client_email: process.env.FIREBASE_CLIENT_EMAIL,
-                private_Key: process.env.FIREBASE_PRIVATE_KEY
+                projectId: process.env.FIREBASE_PROJECT_ID,
+                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+                privateKey: process.env.FIREBASE_PRIVATE_KEY
                     ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
                     : undefined,
             }),
