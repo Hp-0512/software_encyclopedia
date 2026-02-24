82aa1c1b8817a444358dcfea074914f9537b979d
Hp-0512 <hps13145@gmail.com>
2026-02-24 12:33:17 +0530

    Update AdminProfile.jsx

--- Software Encyclopedia_All/Frontend/src/components/AdminProfile.jsx
+++ Software Encyclopedia_All/Frontend/src/components/AdminProfile.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "../css/AdminProfile.css";
+import "../CSS/AdminProfile.css";
 
 export default function AdminProfile() {
   const [admin, setAdmin] = useState(null);
